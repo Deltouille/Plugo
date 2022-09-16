514c0eb33edc8e3294cb9ac68e6a0f72b1b213e5
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2022-09-16 10:40:10 +0200

    Update OfferController.php

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -11,7 +11,6 @@ class OfferController extends AbstractController {
     public function test(){
         $em = new OfferManager();
         $find = $em->find(1);
-        var_dump($find);
         return $this->renderView('offer/index.php', ['offer' => $find]);
     }
 
